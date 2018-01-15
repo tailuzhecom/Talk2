9302f1a571c8a2aaebb9c7618d14614b51195f2c
Unknown <450093877@qq.com>
2018-01-15 16:53:38 +0800

    更新ResponseMessage，增加聊天消息的存储，离线消息的处理

--- ChatServer/Server.go
+++ ChatServer/Server.go
@@ -6,13 +6,21 @@ import (
 	"encoding/json"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 type MessageBody struct {
+	Type string `json:"Type"`
 	From string	`json:"From"`
 	To string	`json:"To"`
 	Content string	`json:"Content"`
 
+}
+
+type ResponseMessage struct {
+	Type string `json:"Type"`
+	Content string `json:"Content"`
+	From string    `json:"From"`
 }
 
 type LoginMessage struct {
@@ -31,12 +39,6 @@ func main()  {
 		fmt.Println("Open db error")
 		return
 	}
-	rows, err := g_db.Query("select * from login where username = ?", "kd")
-	var username_db, passw_db string
-	for rows.Next() {
-		rows.Scan(&username_db, &passw_db)
-		fmt.Println(username_db, passw_db)
-	}
 	listenServer, err := net.Listen("tcp", "127.0.0.1:9999")
 	if err != nil {
 		fmt.Println("Listen error")
@@ -53,6 +55,7 @@ func main()  {
 	}
 }
 
+//登录验证
 func handleLoginConn(conn net.Conn)  {
 	buffer := make([]byte, 2048)
 	var clientName string
@@ -70,15 +73,27 @@ func handleLoginConn(conn net.Conn)  {
 		if rows.Next() {
 			rows.Scan(&username_db, &passw_db)
 			if username_db == loginMessages[0].UserName && passw_db == loginMessages[0].Passwd {
-				conn.Write([]byte("201"))
+				responseMessages := ResponseMessage{}
+				responseMessages.Type = "201"   //登录成功
+				responseMessages.Content = ""
+				jsonStr, _ := json.Marshal(&responseMessages)
+				conn.Write([]byte(jsonStr))
 				g_clientConns[loginMessages[0].UserName] = conn
 				clientName = loginMessages[0].UserName
 				break
 			} else {
-				conn.Write([]byte("404\0"))
+				responseMessages := ResponseMessage{}
+				responseMessages.Type = "404"   //密码不正确，登录失败
+				responseMessages.Content = ""
+				jsonStr, _ := json.Marshal(&responseMessages)
+				conn.Write([]byte(jsonStr))
 			}
 		} else {
-			conn.Write([]byte("404\0"))
+			responseMessages := ResponseMessage{}
+			responseMessages.Type = "404"   //不存在该用户，登录失败
+			responseMessages.Content = ""
+			jsonStr, _ := json.Marshal(&responseMessages)
+			conn.Write([]byte(jsonStr))
 		}
 	}
 	handleReadConn(conn, clientName)
@@ -93,16 +108,65 @@ func handleReadConn(conn net.Conn, clientName string)  {
 			delete(g_clientConns, clientName)
 			return
 		}
+		//解析用户json
 		messageFromClient := []MessageBody{}
 		json.Unmarshal(buffer[0:n], &messageFromClient)
 		fmt.Println(string(buffer[0:n]))
 		fmt.Println("Json from client", messageFromClient)
-		if g_clientConns[messageFromClient[0].To] != nil {
-			g_clientConns[messageFromClient[0].To].Write([]byte(messageFromClient[0].From+ ": " +
-							messageFromClient[0].Content))
-		} else {
-			fmt.Println(messageFromClient[0].To, "该用户不在线")
+		switch messageFromClient[0].Type {
+		case "SendMessage":
+			log.Println(clientName + " SendMessage")
+			if g_clientConns[messageFromClient[0].To] != nil {    //如果用户在线
+				responseMessages := ResponseMessage{}
+				responseMessages.Type = "203"   //发送消息
+				responseMessages.Content = clientName + ": " + messageFromClient[0].Content
+				responseMessages.From = clientName
+				jsonStr, _ := json.Marshal(&responseMessages)
+				g_clientConns[messageFromClient[0].To].Write([]byte(jsonStr))
+			} else {
+				responseMessages := ResponseMessage{}
+				responseMessages.Type = "203"   //发送消息
+				responseMessages.Content = "系统： 该用户暂时不在线"
+				responseMessages.From = messageFromClient[0].To
+				jsonStr, _ := json.Marshal(&responseMessages)
+				g_clientConns[clientName].Write([]byte(jsonStr))
+				//将离线消息存储到数据库
+				stmt, _ := g_db.Prepare(`insert into offline_message(from_, to_, content) values(?, ?, ?)`)
+				stmt.Exec(clientName, messageFromClient[0].To, clientName + ": " + messageFromClient[0].Content)
+				stmt.Close()
+			}
+		case "GetChatRecord":   //获取聊天记录
+			log.Println(clientName + " GetChatRecord")
+			var chatRecord string
+			rows, _ := g_db.Query("select content_ from chatrecord where from_ = ? and to_ = ?", messageFromClient[0].From, messageFromClient[0].To)
+			if rows.Next() {
+				rows.Scan(&chatRecord)
+			}
+			responseMessages := ResponseMessage{}
+			responseMessages.Type = "202"  //聊天记录
+			responseMessages.Content = chatRecord
+			responseMessages.From = messageFromClient[0].To
+			jsonStr, _ := json.Marshal(&responseMessages)
+			g_clientConns[clientName].Write([]byte(jsonStr))
+		case "GetOfflineMessage":   //拉取离线消息
+			//从数据库中获取对方发送给client的消息
+			log.Println(clientName + " :GetOfflineMessage")
+			rows, _ := g_db.Query("select content from offline_message where from_ = ? and to_ = ?",  messageFromClient[0].To, clientName)
+			var content string
+			for rows.Next() {
+				rows.Scan(&content)
+				responseMessages := ResponseMessage{}
+				responseMessages.Type = "203"  //聊天记录
+				responseMessages.Content = content
+				responseMessages.From = messageFromClient[0].To
+				jsonStr, _ := json.Marshal(&responseMessages)
+				g_clientConns[clientName].Write([]byte(jsonStr))
+			}
+			//删除已拉取的消息
+			stmt, _ := g_db.Prepare("DELETE FROM offline_message WHERE from_ = ? and to_ = ?")
+			stmt.Exec(messageFromClient[0].To, messageFromClient[0].From)
 		}
+
 	}
 }
 
--- ThreadTest/mainwindow.h
+++ ThreadTest/mainwindow.h
@@ -4,6 +4,10 @@
 #include <QMainWindow>
 #include <workthread.h>
 #include <QtNetwork/QTcpSocket>
+#include <QListWidgetItem>
+#include <QtSql/QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
 
 #include "logindialog.h"
 
@@ -17,12 +21,14 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    void initDatabase();
     ~MainWindow();
 
 private slots:
     void handleRead();
     void on_send_pushButton_clicked();
     void loginDialog_slot(QString userName, QString passwd);
+    void on_listWidget_itemClicked(QListWidgetItem *item);
 
 private:
     Ui::MainWindow *ui;
@@ -30,6 +36,9 @@ private:
     QTcpSocket* m_socket;
     QString m_userName;
     LoginDialog* m_loginDialog;
+    QMap<QString, QString> m_chatRecord;
+    QSqlDatabase database;
+
 };
 
 #endif // MAINWINDOW_H
